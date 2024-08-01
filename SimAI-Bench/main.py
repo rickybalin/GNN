Fixes for SimAI-Bench GNN on Frontier and initial scaling runs

SimAI-Bench/main.py
@@ -214,9 +214,6 @@ def train(args, model, optimizer, loss_fn, data, comm) -> dict:
         optimizer.step()
         toc_o = perf_counter()
         
-        if args.include_loss_avg=='false':
-            toc_t = perf_counter()
-
         tic_c = perf_counter()
         if size>1:
             if args.device=='xpu':
@@ -232,11 +229,12 @@ def train(args, model, optimizer, loss_fn, data, comm) -> dict:
         toc_c = perf_counter()
         #print(f'rank [{rank}]: backward: {toc_b-tic_b}, metric_avg: {toc_c-tic_c}', flush=True)
 
-        if args.include_loss_avg=='true':
-            toc_t = perf_counter()
-
         if rank==0:
             print(f'[{iteration}]: avg_loss = {loss:>4e}', flush=True)
+       
+        # Timer after the print statement for sync
+        if args.include_loss_avg=='true':
+            toc_t = perf_counter()
 
         if iteration>1:
             t_train = toc_t - tic_t
@@ -336,6 +334,7 @@ if __name__ == '__main__':
     parser.add_argument('--learning_rate', default=0.0001, type=float, help='Optimizer learning rate')
     parser.add_argument('--include_loss_avg', default='true', choices=['true','false'], type=str, help='Include average of the loss across ranks in performance measurement')
     parser.add_argument('--master_addr', default=None, type=str, help='Master address for torch.distributed')
+    parser.add_argument('--master_port', default=None, type=int, help='Master port for torch.distributed')
     args = parser.parse_args()
 
     # Initialize Torch Distributed
@@ -348,14 +347,13 @@ if __name__ == '__main__':
             master_addr = socket.gethostname() if rank == 0 else None
         master_addr = comm.bcast(master_addr, root=0)
         os.environ['MASTER_ADDR'] = master_addr
-        if args.device=='rocm':
-            os.environ['MASTER_PORT'] = str(3442)
+        if args.master_port is not None:
+            os.environ['MASTER_PORT'] = str(args.master_port)
         else:
             os.environ['MASTER_PORT'] = str(2345)
         if (args.device=='cpu'): backend = 'gloo'
         elif (args.device=='cuda'): backend = 'nccl'
         elif (args.device=='xpu'): backend = 'ccl'
-        elif (args.device=='rocm'): backend = 'nccl'
         dist.init_process_group(backend,
                                 rank=int(rank),
                                 world_size=int(size),